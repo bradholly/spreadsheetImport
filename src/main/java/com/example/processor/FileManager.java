079ed878e65ceb8ef963ed7f77602e40484bb5e9
Brad <Brad@DESKTOP-H6T37NV>
2017-08-07 09:38:21 -0500

    working on file manager

--- src/main/java/com/example/processor/FileManager.java
+++ src/main/java/com/example/processor/FileManager.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -18,21 +20,25 @@ import com.opencsv.CSVReader;
 
 @Component
 public class FileManager {
-
+    Logger logger = LoggerFactory.getLogger(FileManager.class);
+	
 	@Autowired
 	FileConverter fileConverter;
 
 	public void processFiles(StorageService storageService) {
+		logger.debug("start processFiles(StorageService storageService)");
+		
 		Stream<Path> stream = storageService.loadAll();
 
 		// iterate files from the storageService object
 		stream.forEach(filename->{
+			logger.debug("the file being processed is " + filename);
 
-			System.out.println(filename);
 			CSVReader csvReader = null;
 			Resource file = storageService.loadAsResource(filename.toString());
 			try {
 				csvReader = new CSVReader(new FileReader(file.getFile()));
+				logger.debug("created csvReader");
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -42,22 +48,25 @@ public class FileManager {
 			}
 
 			if (null == csvReader) {
+				logger.debug("null csvReader, returning");
 				return;
 			}
 
 			// instantiate data object based on filename
 			if (filename.toString().contains("orderheader")) {
+				logger.debug("converting order header type file");
 				List<OrderHeaderFile> orderHeaderFileList = new ArrayList<OrderHeaderFile>();
-				//							new OrderHeaderFile();
-				//					ArrayList<String> columnNames = orderHeaderFile.getColumnNames();
-
-
 				orderHeaderFileList = fileConverter.getOrderHeaderContents(csvReader);
-
+				if (null == orderHeaderFileList) {
+					logger.debug("null orderHeaderFileList returned");
+					return;
+				}
 			}
 
-			//	        CSVIterator
 
 		});
+		
+
+		logger.debug("finish processFiles(StorageService storageService)");
 	}
 }
